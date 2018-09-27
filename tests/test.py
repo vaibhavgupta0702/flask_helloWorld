b0e95b43373b23ee69f40e8d44e84372107b8466
root <root@ip-172-31-30-38.ap-southeast-1.compute.internal>
2018-09-27 10:36:43 +0000

    Committing test cases and requirements_test.txt file

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,25 @@
-#Sample test case file. Will be populated with actual test cases later.
+import unittest
+from urllib.request import urlopen
+from flask import Flask
+from flask_testing import TestCase
+
+class MyTest(TestCase):
+    port = '8080'
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+
+    def test_server_is_up_and_running(self):
+        response = urlopen('http://127.0.0.1:' + str(self.port) )
+        self.assertEqual(response.code, 200)
+
+    def test_server_is_showing_correct_output(self):
+        response = urlopen('http://127.0.0.1:' + str(self.port) )
+        html = str(response.read())
+        output = "b'Hello World! I am running on port " + str(self.port) + "'"
+        self.assertEqual(html,output)
+
+if __name__ == '__main__':
+    unittest.main()
